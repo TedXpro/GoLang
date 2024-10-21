8b5a28995070a667f1d0128efb4806c02a67a745
TedXpro <johannes.woldeyes@gmail.com>
2024-10-21 05:58:29 +0300

    work on practice examples variables and data types

---
+++ practice/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+var name = "Hello";
+
+func main()  {
+	fmt.Println("Hello World!");
+
+	//// strings
+	// var nameOne string = "mario234";
+	// var nameTwo = "Luigi";
+	// var nameThree string;
+
+	// fmt.Println(nameOne, nameTwo, nameThree);
+
+	// nameOne = "Abebe";
+	// nameTwo = "Peach";
+	// nameThree = "Bowser";
+	// fmt.Println(nameOne, nameTwo, nameThree);
+
+	// nameFour := "Yoshi";
+	// fmt.Println(nameFour);
+
+	// nameFour = "Wario";
+	// fmt.Println(nameFour, name);
+
+	//// integers
+	var ageOne int = 20;
+	var ageTwo = 30;
+	ageThree := 40;
+	fmt.Println(ageOne, ageTwo, ageThree);
+
+	// bits and memory
+	// var numOne int8 = 25;
+	// var numTwo int8 = -128;
+	// var numThree uint = 12310231123123123123;
+
+	// fmt.Println(numOne, numThree, numTwo);
+
+	//// Float
+	var scoreOne float64 = 90.23;
+	var scoreTwo = 1230123.123;
+	scoreThree := 8909.123;
+	fmt.Println(scoreOne, scoreTwo, scoreThree);
+}
