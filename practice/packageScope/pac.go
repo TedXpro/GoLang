badac9e6b3ee64f303c5615c5259441be873876d
TedXpro <johannes.woldeyes@gmail.com>
2024-10-23 17:44:52 +0300

    work on packageScopes

---
+++ practice/packageScope/pac.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{10, 20, 30, 40, 50}
+
+func sayHello(str string) {
+	fmt.Println("hello", str);
+}
+
+func showScore(){
+	fmt.Println("socre:", score);
+}
