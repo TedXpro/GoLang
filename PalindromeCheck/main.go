773075783c3988451f95e54b09058a7b5df8e692
TedXpro <johannes.woldeyes@gmail.com>
2024-10-24 10:10:58 +0300

    work on checking if a given string is a palindrome or not

--- PalindromeCheck/main.go
+++ PalindromeCheck/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	sentence := "A man, a plan, a canal, PanaMa!"
+	if(palindromeCheck(sentence)){
+		fmt.Println(sentence, "---> is a plaindrome")
+	}else {
+		fmt.Println(sentence, "---> is not a palindrome")
+	}
+}
+
+func palindromeCheck(sentence string) bool {
+	cleanedString := strings.ToLower(strings.Map(func(r rune) rune {
+		if unicode.IsLetter(r){
+			return r
+		}
+		return -1
+	}, sentence))
+
+	for i := 0; i < len(cleanedString) / 2; i++{
+		if(cleanedString[i] != cleanedString[len(cleanedString) - i - 1]){
+			return false
+		}
+	}
+
+	return true
+}
